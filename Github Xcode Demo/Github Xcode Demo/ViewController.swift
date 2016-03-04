a691562bc0ba2dbb3e1e4c4ab14317adfc539834
Rob <Home@iMac.local>
2016-03-03 19:49:05 -0500

    Added our Xcode project
    Created a template single view application

---
+++ Github Xcode Demo/Github Xcode Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Github Xcode Demo
+//
+//  Created by Rob on 2016-03-3.
+//  Copyright © 2016 loungingrabbit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
